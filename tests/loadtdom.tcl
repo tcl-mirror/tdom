ce1296a61d632c7221b59d5c8ad410316b49e91b
rolf <rolf>
2017-08-17 18:44:23 +0000

    Changed my mind: next version will be 0.9.0.

--- tests/loadtdom.tcl
+++ tests/loadtdom.tcl
@@ -8,7 +8,7 @@
 
 package require tcltest
 namespace import ::tcltest::*
-package require -exact tdom 0.8.4
+package require -exact tdom 0.9.0
 if {[lsearch [namespace children] ::tDOM] == -1} {
     # tcldomsh without the script library. Source the lib.
     source [file join [file dir [info script]] ../lib tdom.tcl]
--- win/pkgIndex.tcl
+++ win/pkgIndex.tcl
@@ -1,5 +1,5 @@
 # tDOM Tcl package index file
 
-package ifneeded tdom 0.8.4 \
-    "[list load   [file join $dir tdom084[info sharedlibextension] ] tdom];\
+package ifneeded tdom 0.9.0 \
+    "[list load   [file join $dir tdom090[info sharedlibextension] ] tdom];\
      [list source [file join $dir tdom.tcl]]"
