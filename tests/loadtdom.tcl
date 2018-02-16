88b2bba97e6d8bd00938ed5cdc6d15ab86febd9d
rolf <rolf>
2018-02-16 01:59:12 +0000

    Needed another check about loading the script library, because with ::tDOM::pullparser the ::tDOM namespace is always there.

--- tests/loadtdom.tcl
+++ tests/loadtdom.tcl
@@ -9,7 +9,7 @@
 package require tcltest
 namespace import ::tcltest::*
 package require -exact tdom 0.9.1
-if {[lsearch [namespace children] ::tDOM] == -1} {
+if {[info commands ::tDOM::xmlReadFile] eq ""} {
     # tcldomsh without the script library. Source the lib.
     source [file join [file dir [info script]] ../lib tdom.tcl]
 }
